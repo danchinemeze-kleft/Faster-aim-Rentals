8134b81d7938cc3aacbe4fbf1748958133ae5511
Daniel Igboke <danchinemeze@gmail.com>
2026-06-07 11:20:06 +0100

    feat: add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,8 @@
+﻿export default function sitemap() {
+  return [
+    { url: 'https://rent.fasteraim.com', lastModified: new Date() },
+    { url: 'https://rent.fasteraim.com/browse', lastModified: new Date() },
+    { url: 'https://rent.fasteraim.com/search', lastModified: new Date() },
+    { url: 'https://rent.fasteraim.com/list', lastModified: new Date() },
+  ]
+}
