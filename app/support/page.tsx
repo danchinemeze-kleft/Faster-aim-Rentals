178c955f3876b6be7277029f179eed8206e8cda0
Daniel Igboke <danchinemeze@gmail.com>
2026-07-11 09:30:24 +0100

    Fix missing anchor tag in support page

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -8,7 +8,7 @@ export default function SupportPage() {
         Having an issue? Reach out and we'll get back to you shortly.
       </p>
       
-        href="mailto:Info@fasteraim.com?subject=Mr.%20Rent%20Support%20Enquiry&body=Please%20describe%20the%20issue%20and%20attach%20screenshot%20of%20the%20affected%20page%3A%0A%0A"
+       <a href="mailto:Info@fasteraim.com?subject=Mr.%20Rent%20Support%20Enquiry&body=Please%20describe%20the%20issue%20and%20attach%20screenshot%20of%20the%20affected%20page%3A%0A%0A"
         style={{
           display: "inline-block",
           padding: "12px 24px",
